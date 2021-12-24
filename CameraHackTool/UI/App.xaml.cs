9a7c013b2ee8f67859cc4ddca802bfeb3e91e5af
trotlinebeercan <hopper.dustin@gmail.com>
2021-12-23 20:56:39 -0700

    WinProj: Readding Fody because all this AppStore stuff is insane

--- CameraHackTool/UI/App.xaml.cs
+++ CameraHackTool/UI/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace CameraHackTool
 {
